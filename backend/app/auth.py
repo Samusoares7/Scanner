fb02a3a6ca6aaac9ca9eb3730c8e4f7d8873ee39
Samusoares7 <samuraygk@gmail.com>
2026-04-29 16:33:44 -0300

    security: mudança SECRET_KEY

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -3,8 +3,9 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+import os
 
-SECRET_KEY = "scanner-pro-secret-key-2026"
+SECRET_KEY = os.getenv("SECRET_KEY", "scanner-pro-secret-key-dev")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
